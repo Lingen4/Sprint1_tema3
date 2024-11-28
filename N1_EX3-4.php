785a440532333e3cd588e4ea7c5c0c0725bad0fb
Lingen4 <lucrfb@gmail.com>
2024-11-28 13:41:52 +0100

    ejercicios n2 ex2, n3 ex1

--- N1_EX3-4.php
+++ N1_EX3-4.php
@@ -2,10 +2,9 @@
 // *************************** NIVEL 1 ***************************
 //EJERCICIO 3
 
-$palabras = array('hola', 'arbol');
+$palabras = array('hola', 'arbol', 'rama');
 
-$caracter = readline("Que caracter quieres introducir: ");
-$caracter = $caracter[0];
+$caracter = readline("Que caracter quieres introducir: ")[0];
 
 function comprobarLetra($palabras, $caracter){
 
@@ -21,6 +20,7 @@ $bolean = comprobarLetra($palabras, $caracter);
 var_dump($bolean);
 
 //EJERCICIO 4
+echo "\n";
 $misDatos = array("nombre"=> "Luciano", "edad"=> 29, "email" => "lucrfb@gmail.com", "comida favorita" => "pizza");
 
 var_dump($misDatos);
--- N2_EX2.php
+++ N2_EX2.php
@@ -3,23 +3,49 @@
 //EJERCICIO 2
 
 $alumnos = [
-
-    "Pedro" => ["notas" => [2, 6, 7, 8, 6]],
-    "Maria" => ["notas" => [3, 5, 6, 9, 8]],
-    "Miguel" => ["notas" => [4, 7, 6, 5, 10]]
-
+    "Pedro" =>  [2, 6, 7, 8, 6],
+    "Maria" =>  [3, 5, 6, 9, 8],
+    "Miguel" => [4, 7, 6, 5, 10]
 ];
 
-/*var_dump($alumnos);
-$sum = array_reduce($alumnos,$);*/
+/* SI QUEREMOS AGREGAR UN ALUMNO MANUALMENTE
 
+function agregarAlumno (&$alumnos){
 
-function notas($alumnos)
-{
+$nombreNuevo=readline("Nombre del alumno: ");
 
-    foreach ($alumnos as $persona) {
-        $notas = $persona["notas"];
-
-        echo $notas;
-    }
+$notasAlumno = [];
+for ((int) $i=1; $i<=5; $i++){
+$nota= (int) readline("Escriba la $i º nota--> ");
+array_push($notasAlumno,$nota);
 }
+
+$alumnos[$nombreNuevo]=$notasAlumno;}
+
+*/
+
+function promedio($alumnos){
+
+    $notaClase=0;
+
+    foreach ($alumnos as $persona => $notas) {
+
+        $notasTotal = array_sum($notas);
+
+        $cantidadNotas = count($notas);       
+        
+        $promedioIndividual= $notasTotal/$cantidadNotas;
+
+       echo  "\n$persona tiene un promedio de: ",  $promedioIndividual;
+    
+       $notaClase += $promedioIndividual;}
+
+       echo "\nLa nota media de la clase es de: ", $notaClase/count($alumnos);
+    
+    }
+
+//echo agregarAlumno($alumnos);
+
+echo promedio($alumnos);
+
+?>
